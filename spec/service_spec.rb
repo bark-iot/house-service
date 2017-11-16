9cecdadb568c564b4bb48b1e983c230da47919aa
Serge Koba <s.koba@mobidev.biz>
2017-11-16 16:58:07 +0200

    add bad token tests

--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -4,7 +4,11 @@ describe 'Houses Service' do
   before(:each) do
     DB.execute('TRUNCATE TABLE houses;')
     stub_request(:get, 'http://lb/users/by_token').
+        with(headers: {'Authorization'=>"Bearer #{token}"}).
         to_return(status: 200, body: '{"id":1,"email":"test@test.com","token":"a722658b-0fea-415c-937f-1c1d3c8342fd","created_at":"2017-11-14 16:06:52 +0000","updated_at":"2017-11-14 16:06:52 +0000"}', headers: {})
+    stub_request(:get, 'http://lb/users/by_token').
+        with(headers: {'Authorization'=>'Bearer wrong_token'}).
+        to_return(status: 422, body: '', headers: {})
   end
 
   it 'should list all houses for user' do
@@ -15,6 +19,13 @@ describe 'Houses Service' do
     expect(last_response).to be_ok
     body = JSON.parse(last_response.body)
     expect(body[0]['title'] == house_title).to be_truthy
+  end
+
+  it 'should not list all houses for user with wrong token' do
+    header 'Authorization', 'Bearer wrong_token'
+    get '/houses'
+
+    expect(last_response.status).to equal(401)
   end
 
   def token
