3774cc8a95cbe344ea326318a76bfa2b7b61a148
Serge Koba <s.koba@mobidev.biz>
2018-01-13 21:35:59 +0200

    optimization

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker-compose run house-service bundle
-docker-compose run house-service ./cli db_migrate
+docker-compose run house-service bundle exec ./cli db_migrate
 cd ../house-service/docs && mkdocs build # build api doc
